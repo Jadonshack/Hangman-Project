c6d8eb146868d97fc03ad6d240ecf713c15367b8
Jadon Shackelford <shackelfordjadon@gmail.com>
2022-09-03 22:27:55 -0700

    added wrong letters and dynamic hangman plus minor changes

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Stickman array
+var stickman = [];
+var stickmanAlive = ["1"];
+var answer = ["1"];
+
 //Guessing button
 var btn =document.getElementById('btn');
 btn.addEventListener("click", function(event) {
@@ -6,47 +11,92 @@ btn.addEventListener("click", function(event) {
     let letter1 = document.getElementById('input1').value;
 
     // If statement to check if letter D is correct
-    if (letter1 == 'd') {
-        letterD.innerHTML = "D"; 
+    if(answer.length < 4) {
+        if(stickmanAlive.length == 1) {
+            if (letter1 == 'd') {
+                letterD.innerHTML = "D"; 
+                answer += "1"
+            } else {
+            }
+            if (letter1 == 'D') {
+                letterD.innerHTML = "d"; 
+                answer += "1"
+            } else {
+            }
+            // If statement to check if letter O is correct
+            if (letter1 == 'o') {
+                letterO.innerHTML = "o"; 
+                answer += "1"
+            } else {
+            }
+            if (letter1 == 'O') {
+                letterO.innerHTML = "o"; 
+                answer += "1"
+            } else {
+            }
+            // If statement to check if letter is G correct
+            if (letter1 == 'g') {
+                letterG.innerHTML = "g"; 
+                answer += "1"
+            } else {
+            }
+            if (letter1 == 'G') {
+                letterG.innerHTML = "g"; 
+                answer += "1"
+            } else {
+            }
+        } else {
+        }
     } else {
     }
-    if (letter1 == 'D') {
-        letterD.innerHTML = "d"; 
+    console.log(letter1.length);
+    // Switch to alert player when they guess wrong and adds to var stickman length
+    if (answer.length < 4) {
+        if (stickmanAlive.length == 1) {
+            if (letter1.length != 1) {
+            } else {
+                switch(letter1) {
+                    case "d": 
+                        break;
+                    case "D": 
+                        break;
+                    case "o": 
+                        break;
+                    case "O": 
+                        break;
+                    case "g": 
+                        break;
+                    case "G": 
+                        break;
+                    default:
+                        stickman += "1";
+                        alert("Oops, try again!");
+                        wrongTitle.innerHTML += " ";
+                        wrongTitle.innerHTML += letter1;
+                    }
+            }
+        } else {
+        }
     } else {
+        alert("Congradulations! You saved the stick man!");
     }
-    // If statement to check if letter O is correct
-    if (letter1 == 'o') {
-        letterO.innerHTML = "o"; 
-    } else {
-    }
-    if (letter1 == 'O') {
-        letterO.innerHTML = "o"; 
-    } else {
-    }
-    // If statement to check if letter is G correct
-    if (letter1 == 'g') {
-        letterG.innerHTML = "g"; 
-    } else {
-    }
-    if (letter1 == 'G') {
-        letterG.innerHTML = "g"; 
-    } else {
-    }
-    // Switch to alert player when they guess wrong
-    switch(letter1) {
-        case "d": 
-            break;
-        case "D": 
-            break;
-        case "o": 
-            break;
-        case "O": 
-            break;
-        case "g": 
-            break;
-        case "G": 
-            break;
-        default:
-            alert("Oops, try again!");
-    }
+    // makes stickman appear when player answers wrong
+    if (answer.length < 4) {
+        switch(stickman.length) {
+            case 1:
+                head.innerHTML = "o";
+                break;
+            case 2:
+                torso.innerHTML = "|";
+                break;
+            case 3:
+                arms.innerHTML = "_";
+                break;
+            case 4:
+                legs.innerHTML = "^";
+                alert("Oh no! You couldn't save them.");
+                stickmanAlive += "1";
+                break;
+        }
+    }   
     });
